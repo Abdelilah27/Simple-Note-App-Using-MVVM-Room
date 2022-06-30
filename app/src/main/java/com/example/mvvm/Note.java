4ea011af3fc2f0e91275ea4f965e96e1653aa7d8
Abdelilah <abdelilah.ngadi27@gmail.com>
2022-06-30 21:49:08 +0100

    First Commit - Create Class - Create Dao

---
+++ app/src/main/java/com/example/mvvm/Note.java
@@ -0,0 +1,47 @@
+package com.example.mvvm;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "note_table")
+public class Note {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String title;
+    private String description;
+    private String priority;
+
+    public Note(String title, String description, String priority) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+}
