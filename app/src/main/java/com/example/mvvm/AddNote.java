949d580d17f41f83e005725ae0d61ccafeaf3e57
Abdelilah <abdelilah.ngadi27@gmail.com>
2022-07-01 19:24:32 +0100

    Create Add Note Class

--- app/src/main/java/com/example/mvvm/AddNote.java
+++ app/src/main/java/com/example/mvvm/AddNote.java
@@ -1,14 +1,66 @@
 package com.example.mvvm;
 
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class AddNote extends AppCompatActivity {
+
+    private EditText ediTextTitle, ediTextDescription;
+    private NumberPicker priorityPicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
+
+        ediTextTitle = findViewById(R.id.ediTextTitle);
+        ediTextDescription = findViewById(R.id.ediTextDescription);
+        priorityPicker = findViewById(R.id.priorityPicker);
+
+        priorityPicker.setMinValue(1);
+        priorityPicker.setMaxValue(10);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_close_24);
+        setTitle("Add Note");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.saveNoteItem:
+                saveNote();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void saveNote() {
+        String title = ediTextTitle.getText().toString();
+        String description = ediTextDescription.getText().toString();
+        int priority = priorityPicker.getValue();
+
+        if (title.trim().isEmpty() || description.trim().isEmpty()) {
+            Toast.makeText(this, "Please insert all the fields", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
     }
 }
